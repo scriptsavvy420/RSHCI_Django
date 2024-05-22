b22c77f0e16c808a00319b64f648cc86903b9096
ScriptSavvy <akira.murayama.dev@gmail.com>
2024-05-23 02:35:36 +0900

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('homepage.urls')),
-    path('cms/', include('admin.urls')),
+    path('cms/', include(admin.urls)),
     path('admin/', include('custom_admin.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
